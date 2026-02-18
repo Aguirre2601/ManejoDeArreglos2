49671bf9298b8f19c088ccd4c7de6b98a9f28534
MarielaAguirre <marcoronelgege@gmail.com>
2026-02-18 01:44:22 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System; 
+
+namespace ManejoDeArreglos2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Variables.Array = new int[3, 5];
+            Variables.Array2 = new int[5, 3];
+            Variables.var_num = new Random();
+            for (int f = 0; f <= 2; f++)
+            {
+                for (int c = 0; c <= 4; c++)
+                {
+                    Variables.Array[f, c] = Variables.var_num.Next(0, 9);
+                    Console.Write(" " + Variables.Array[f, c]);
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n\n");
+            for (int f = 0; f <= 4; f++)
+            {
+                for (int c = 0; c <= 2; c++)
+                {
+                    Variables.Array2[f, c] = Variables.Array[c, f];
+                    Console.Write(" " + Variables.Array2[f, c]);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Presione cualquier tecla para SALIR");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+namespace ManejoDeArreglos2
+{
+    public class Variables
+    {
+        public static int[,] Array;
+        public static int[,] Array2;
+        public static Random var_num;
+
+    }
+}
